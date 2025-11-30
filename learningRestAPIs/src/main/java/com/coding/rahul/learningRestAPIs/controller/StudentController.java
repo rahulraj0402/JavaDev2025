c7c399ba26886ebcc7ecbc590b2c47d3f8fd4c75
rahulraj0402 <rahul3004rt@gmail.com>
2025-11-30 15:55:45 +0530

    connected presentation layer with persistance layer with jpa,postgres database

--- learningRestAPIs/src/main/java/com/coding/rahul/learningRestAPIs/controller/StudentController.java
+++ learningRestAPIs/src/main/java/com/coding/rahul/learningRestAPIs/controller/StudentController.java
@@ -1,14 +1,25 @@
 package com.coding.rahul.learningRestAPIs.controller;
 
 import com.coding.rahul.learningRestAPIs.dto.StudentDto;
+import com.coding.rahul.learningRestAPIs.entity.Student;
+import com.coding.rahul.learningRestAPIs.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent(){
-       return new StudentDto(12L,"Rahul raj", "rahul3004rt@gmail.com");
+    public List<Student> getStudent(){
+       return studentRepository.findAll();
     }
 
     @GetMapping("/student/{id}")
---
+++ learningRestAPIs/src/main/java/com/coding/rahul/learningRestAPIs/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.coding.rahul.learningRestAPIs.repository;
+
+import com.coding.rahul.learningRestAPIs.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student , Long> {
+}
